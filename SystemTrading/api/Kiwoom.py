21b6c378027f93202b1dcab409f9f269b0205c5d
ProgrammerKim <baram918@naver.com>
2025-06-03 21:48:59 +0900

    가격정보 가져오기 수정본250603

--- SystemTrading/api/Kiwoom.py
+++ SystemTrading/api/Kiwoom.py
@@ -9,9 +9,9 @@ class Kiwoom(QAxWidget):
         super().__init__()
         self._make_kiwoom_instance()
         self._set_signal_slots()
-        self._comm_connect()
+        self._comm_connect()#로그인 요청하는 메소드 시작
 
-        self.account_number = self.get_account_number()
+        self.account_number = self.get_account_number()#계좌번호 가져옴
 
         self.tr_event_loop = QEventLoop()#TR 요청에 대한 응답 대기를 위한 변수 데이터가 올 때까지 기다렸다가 처리해!" 하는 용도로 사용하는 이벤트 루프
     def _make_kiwoom_instance(self): #키움 클래스가 API를 사용할 수 있도록 등록하는 함수
@@ -24,7 +24,7 @@ class Kiwoom(QAxWidget):
         self.OnEventConnect.connect(self._login_slot)
 
         #TR의 응답결과를 받도록 설정
-#        self.OnReceiveTrData.connect(self._on_recive_tr_data)
+        self.OnReceiveTrData.connect(self._on_receive_tr_data)
 
     #로그인 성공시 0 실패시 다른값이 나옴 100, 101, 102 등
     def _login_slot(self, err_code):
@@ -62,22 +62,25 @@ class Kiwoom(QAxWidget):
         return code_name
 
     def get_price_data(self, code):
+        #종목의 상장일부터 가장 최근 일자까지 일봉정보를 가져오는 함수
         self.dynamicCall("SetInputValue(QString, QString", "종목코드", code)
         self.dynamicCall("SetInputValue(QString, QString", "수정주가구분", 1)#1이면 수정주가 적용함
         self.dynamicCall("CommRqData(QString, QString, int, QString", "opt10081_req", "opt10081", 0, "0001")
 
-        self.tr_event_loop.exec_()#TR요청보낸후 응답 대기상태로 만드는 코드임 이후 코드는 TR에 대한 응답이 도착한 후 실행됨
+        self.tr_event_loop.exec_()#TR요청 보낸후 응답 대기상태로 만드는 코드임 이후 코드는 TR에 대한 응답이 도착한 후 실행됨
 
         ohlcv = self.tr_data#응답 slot함수 _on_receive_tr_data에서 수신한 일봉 데이터 저장되어있어서 사용 가능함
 
-        while self.has_next_tr_data:
+        while self.has_next_tr_data: #다음 데이터가 있다면 실행
             self.dynamicCall("SetInputValue(QString, QString", "종목코드", code)
             self.dynamicCall("SetInputValue(QString, QString", "수정주가구분", 1)  # 1이면 수정주가 적용함
-            self.dynamicCall("CommRqData(QString, QString, int, QString", "opt10081_req", "opt10081", 2, "0001")
-            self.tr_event_loop.exec_()
+            self.dynamicCall("CommRqData(QString, QString, int, QString", "opt10081_req", "opt10081", 2, "0001")#2인 이유는 연속조회이므로
+            self.tr_event_loop.exec_()#TR요청 보낸후 응답 대기상태로 만드는 코드임 이후 코드는 TR에 대한 응답이 도착한 후 실행됨
 
-            for key, val in self.tr_data_items():
+            #최초로 얻어온 가격 데이터를 담은 ohlcv에 데이터를 이어 붙이는 작업을 함
+            for key, val in self.tr_data.items():
                 ohlcv[key] += val
+
 
         df = pd.DataFrame(ohlcv, columns=['open', 'high', 'low', 'close', 'volume'], index=ohlcv['date'])
 
@@ -96,15 +99,15 @@ class Kiwoom(QAxWidget):
             self.has_next_tr_data = False
 
         if rqname == "opt10081_req":
-            ohlcv = {'date': [], 'open': [], 'high': [], 'low': [], 'close': [], 'volume': []}
+            ohlcv = {'date': [], 'open': [], 'high': [], 'low': [], 'close': [], 'volume': []}#ohlcv는 지역변수임
 
             for i in range(tr_data_cnt):
                 date = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "일자")
-                open = self.dynamicCall('GetCommData(QString, QString, int, QString', trcode, rqname, i, "시가")
-                high = self.dynamicCall('GetCommData(QString, QString, int, QString', trcode, rqname, i, "고가")
-                low = self.dynamicCall('GetCommData(QString, QString, int, QString', trcode, rqname, i, "저가")
-                close = self.dynamicCall('GetCommData(QString, QString, int, QString', trcode, rqname, i, "현재가")
-                volume = self.dynamicCall('GetCommData(QString, QString, int, QString', trcode, rqname, i, "거래량")
+                open = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "시가")
+                high = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "고가")
+                low = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "저가")
+                close = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "현재가")
+                volume = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "거래량")
 
                 ohlcv['date'].append(date.strip())
                 ohlcv['open'].append(int(open))
@@ -113,7 +116,25 @@ class Kiwoom(QAxWidget):
                 ohlcv['close'].append(int(close))
                 ohlcv['volume'].append(int(volume))
 
-            self.tr_data = ohlcv
+            self.tr_data = ohlcv#글로벌 변수로 편입시킴
+
+        #예수금 얻어오기 받는 부분
+        elif rqname == "opw00001_req":
+            deposit = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, 0, "주문가능금액")
+            self.tr_data = int(deposit)
+            print(self.tr_data)
 
         self.tr_event_loop.exit()#tr 요청을 보내고 응답을 대기시키는데 사용하는 self.tr_event_loop 를 종료 시킴
         time.sleep(0.5)#0.5초만듬 쉼
+
+    #조회 대상 계좌의 예수금을 얻어 오는 함수
+    def get_deposit(self):
+        self.dynamicCall("SetInputValue(QString, QString","계좌번호", self.account_number)
+        self.dynamicCall("SetInputValue(QString, QString", "비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(QString, QString",  "조회구분", "2")
+        self.dynamicCall("CommRqData(QString, QString, int, QString","opw00001_req", "opw00001", 0, "0002")
+
+        self.tr_event_loop.exec()#응답을 대기할 수 있는 상태로 만듬
+        return self.tr_data
+            
+
--- SystemTrading/main.py
+++ SystemTrading/main.py
@@ -5,7 +5,11 @@ app = QApplication(sys.argv)
 kiwoom = Kiwoom()
 #kiwoom.get_account_number()
 
-kospi_code_list = kiwoom.get_code_list_by_market("0")
+
+df = kiwoom.get_price_data("005930_AL")
+print(df)
+
+'''kospi_code_list = kiwoom.get_code_list_by_market("0")
 print(len(kospi_code_list))
 
 for code in kospi_code_list:
@@ -16,7 +20,7 @@ kosdaq_code_list = kiwoom.get_code_list_by_market("10")
 print(kosdaq_code_list)
 for code in kosdaq_code_list:
     code_name = kiwoom.get_master_code_name(code)
-    print(code, code_name)
+    print(code, code_name)'''
 
 app.exec_()
 
